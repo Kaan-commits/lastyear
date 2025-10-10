c77e1ece578572086ad82f85380e817f0015b3f1
Kaan Kara <kaan.kara@yahoo.com>
2025-10-10 09:56:15 +0300

    Convert lastyear to regular folder and add project files

---
+++ lastyear/degiskenler.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace degiskenler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Değişkenleri ekrana yazdırma
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
