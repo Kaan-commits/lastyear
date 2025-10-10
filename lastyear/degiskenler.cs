15da00ed828b8ad22f3e241ebad03c3a75f51b97
Kaan Kara <kaan.kara@yahoo.com>
2025-10-10 10:00:32 +0300

    Update degiskenler.cs

--- lastyear/degiskenler.cs
+++ lastyear/degiskenler.cs
@@ -6,9 +6,19 @@ namespace degiskenler
     {
         static void Main(string[] args)
         {
-
-            // Değişkenleri ekrana yazdırma
+            //açıklama satırı
+            /*
+             Çoklu açıklama satırı
+            */
+            // console'a ekrana yazdırma
             Console.WriteLine("Hello World!");
+
+            // değişken tanımlama ve console'a yazdırma
+            int sayı = 12;
+            char karakter = 'A';
+
+            Console.WriteLine(sayı);
+            Console.WriteLine(karakter);
         }
     }
 }
