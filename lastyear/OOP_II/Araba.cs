707d8978e52710fac31ddec8d679ff530599d4e1
Kaan Kara <kaan.kara@yahoo.com>
2025-10-17 11:55:10 +0300

    anladı hoca

---
+++ lastyear/OOP_II/Araba.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_II
+{
+    internal class Araba
+    {
+        protected int hiddenData;
+        public string Plaka { get; set; }
+        public string Model { get; set; }
+    }
+}
---
+++ lastyear/OOP_II/Program.cs
@@ -0,0 +1,9 @@
+using System.Runtime;
+using OOP_II;
+
+Console.WriteLine("Kalıtım-CokSekillilik");
+
+Araba araba = new Araba();
+Ferrari ferrari = new Ferrari();
+
+ferrari.
